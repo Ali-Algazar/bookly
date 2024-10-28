5634e81645aa745283376d961459cda74a672297
Ali Algazar <ila3omo@gmail.com>
2024-10-28 11:39:33 +0300

    create text theme class

---
+++ lib/core/themes/text_Theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextTheme {
+  static const TextStyle textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyle20 = TextStyle(
+      fontSize: 20, fontWeight: FontWeight.w400, fontFamily: 'GT Sectra Fine');
+  static const TextStyle textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyle15 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyle30 = TextStyle(
+      fontSize: 30, fontWeight: FontWeight.w400, fontFamily: 'GT Sectra Fine');
+  static const TextStyle textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Gilroy',
+  );
+}
