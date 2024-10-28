0e21e055eac945cc506d2934ac7cf2d8eb807d60
Ali Algazar <ila3omo@gmail.com>
2024-10-28 12:57:41 +0300

    create home class

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,8 +1,10 @@
+import 'package:bookly/featureds/home/presentation/views/home_view.dart';
 import 'package:bookly/featureds/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   static final Map<String, Widget Function(BuildContext)> routes = {
     SplashView.id: (context) => const SplashView(),
+    HomeView.id: (context) => const HomeView(),
   };
 }
