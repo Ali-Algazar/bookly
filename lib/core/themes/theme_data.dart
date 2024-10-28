042df9cd03d30e863db93804e4a17dcecd85b1d8
Ali Algazar <ila3omo@gmail.com>
2024-10-28 11:24:00 +0300

    add google font packages & add TextTheme in themedata

--- lib/core/themes/theme_data.dart
+++ lib/core/themes/theme_data.dart
@@ -1,9 +1,11 @@
 import 'package:bookly/core/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppThemeData {
   static final lightTheme = ThemeData(
+      textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       colorScheme: const ColorScheme.light(
-    background: prmaryColor,
-  ));
+        background: prmaryColor,
+      ));
 }
