570e0f4a560914392e641cbba669b848bed5e724
Ali Algazar <ila3omo@gmail.com>
2024-10-29 16:55:12 +0300

    edit in Book detiles

--- lib/featureds/home/presentation/views/widget/book_detiles_body.dart
+++ lib/featureds/home/presentation/views/widget/book_detiles_body.dart
@@ -1,4 +1,7 @@
+import 'package:bookly/core/themes/text_Theme.dart';
 import 'package:bookly/featureds/home/presentation/views/widget/custom_book_detiles_appber.dart';
+import 'package:bookly/featureds/home/presentation/views/widget/retting_widget.dart';
+import 'package:bookly/featureds/home/presentation/views/widget/slider_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetelsBody extends StatelessWidget {
@@ -6,12 +9,47 @@ class BookDetelsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        SizedBox(
+        const SizedBox(
           height: 40,
         ),
-        CustomBookDetilesAppBer(),
+        const CustomBookDetilesAppBer(),
+        const SizedBox(
+          height: 25,
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.308641975308642,
+          child: const ImageItem(),
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Text(
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            'The Jungle Book',
+            style: FontTheme.textStyle30,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          'Rudyard Kipling',
+          style: FontTheme.textStyle16.copyWith(
+            fontWeight: FontWeight.normal,
+            color: Colors.white.withOpacity(0.7),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const CustomRetting(),
       ],
     );
   }
