8a75ecc0d81244752aa2a984f742e1e75d860b2f
Ali Algazar <ila3omo@gmail.com>
2024-10-29 11:41:43 +0300

    add text in home body

--- lib/featureds/home/presentation/views/widget/home_view_body.dart
+++ lib/featureds/home/presentation/views/widget/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/themes/text_Theme.dart';
 import 'package:bookly/featureds/home/presentation/views/widget/custom_home_appber.dart';
 import 'package:bookly/featureds/home/presentation/views/widget/slider_list_view.dart';
 import 'package:flutter/material.dart';
@@ -8,12 +9,24 @@ class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         CustomHomeAppBer(),
         SizedBox(
           height: 45,
         ),
         SliderListView(),
+        SizedBox(
+          height: 40,
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 30),
+          child: Text(
+            'Best Seller',
+            textAlign: TextAlign.left,
+            style: FontTheme.textStyle18,
+          ),
+        ),
       ],
     );
   }
