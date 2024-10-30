7700052f23ddf0b0b6fe08a71f0121ffac91f89a
Ali Algazar <ila3omo@gmail.com>
2024-10-30 18:19:03 +0300

    create searchButtom

--- lib/featureds/search/presentation/view/widget/Search_body.dart
+++ lib/featureds/search/presentation/view/widget/Search_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/themes/text_Theme.dart';
 import 'package:bookly/featureds/search/presentation/view/widget/custom_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -7,13 +8,40 @@ class SearchBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
       child: Column(
         children: [
-          CustomTextField(),
+          const SearchButtom(),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: double.maxFinite,
+            height: 55,
+            child: TextButton(
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.white)),
+              onPressed: () {},
+              child: Text(
+                'Search',
+                style: FontTheme.textStyle16.copyWith(color: Colors.black),
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+}
+
+class SearchButtom extends StatelessWidget {
+  const SearchButtom({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomTextField();
   }
 }
