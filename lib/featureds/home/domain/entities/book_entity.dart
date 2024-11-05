4cf37d4a26ee87c6a648f832f9019c3c710976bd
Ali Algazar <ila3omo@gmail.com>
2024-11-05 08:30:08 +0200

    Create book entity

---
+++ lib/featureds/home/domain/entities/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String imgUrl;
+  final String autherName;
+  final String titel;
+  final num price;
+  final num rating;
+
+  BookEntity({
+    required this.imgUrl,
+    required this.autherName,
+    required this.titel,
+    required this.price,
+    required this.rating,
+  });
+}
